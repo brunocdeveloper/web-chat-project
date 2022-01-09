4f4b1f1040c4944239a5b54bef6198a8153afcbf
Bruno <brunocmoraes97@gmail.com>
2022-01-09 19:15:33 -0300

    Req 4

--- server.js
+++ server.js
@@ -19,20 +19,37 @@ const { getDate } = require('./helpers/actualDate');
 const { getAllMessages } = require('./controllers/history');
 const { createHistory } = require('./models/history');
 
-app.get('/', getAllMessages);
-
 app.use(cors());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+const users = {};
+
 io.on('connection', (socket) => {
   const currentDate = getDate();
-  socket.emit('greetings', '');
+  users[socket.id] = socket.id.substr(0, 16);
+
+  socket.emit('firstNameUser', users[socket.id]);
+
+  io.emit('onlineUser', users);
+  
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+
+    io.emit('onlineUser', users);
+  });
 
   socket.broadcast.emit('newConnection', 'Alguém acabou de se conectar');
-
+  
   socket.on('message', async ({ chatMessage, nickname }) => {
     await createHistory(chatMessage, nickname, currentDate);
     io.emit('message', `${currentDate} ${nickname}: ${chatMessage}`);
   });
+
+  socket.on('attNickName', (newName) => {
+    users[socket.id] = newName;
+    io.emit('onlineUser', users);
+  });
 });
+
+app.get('/', getAllMessages);
